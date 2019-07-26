170fe8bc32a54f12dcba7ab4fce058b841b06a95
EcarusRW <albertoveoveo@gmail.com>
2019-07-26 16:50:55 +0200

    Collisions fix

--- core/Player.gd
+++ core/Player.gd
@@ -4,8 +4,9 @@ export (int) var speed = 50
 export (int) var step = 64
 
 var direction = Vector2()
-onready var root=$"."
 var moving=false
+onready var root=$"."
+onready var grid=get_parent()
 
 func _ready():
 	pass
@@ -14,22 +15,23 @@ func get_input():
 		moving=true
 		direction = Vector2()
 		if Input.is_action_pressed('ui_right'):
-			direction.x += step
+			direction.x += 1
 		if Input.is_action_pressed('ui_left'):
-			direction.x -= step
+			direction.x -= 1
 		if Input.is_action_pressed('ui_down'):
-			direction.y += step
+			direction.y += 1
 		if Input.is_action_pressed('ui_up'):
-			direction.y -= step
-			is_hitting_wall(Vector2(0,-step))
-		#direction = direction.normalized() * speed
-		$Tween.interpolate_property(root,"position",root.position,root.position+direction,10.0/speed,Tween.TRANS_LINEAR,Tween.EASE_IN)
-		$Tween.start()
+			direction.y -= 1
+		request_move()
 
 func _physics_process(delta):
 	get_input()
-	
-func is_hitting_wall(direction):
-	$Label.rect_position+=direction
+
+func request_move():
+	if !grid.hitting_wall(self,direction):
+		$Tween.interpolate_property(root,"position",root.position,root.position+direction*step,10.0/speed,Tween.TRANS_LINEAR,Tween.EASE_IN)
+	else:
+		$Tween.interpolate_property(root,"position",root.position,root.position,10.0/speed,Tween.TRANS_LINEAR,Tween.EASE_IN)
+	$Tween.start()
 func _on_Tween_tween_completed(object, key):
 	moving=false
--- map/TestMap.gd
+++ map/TestMap.gd
@@ -1,4 +1,10 @@
-extends Node2D
+extends TileMap
 
-func _ready():
-	pass
+func hitting_wall(player,direction):
+	var target_cell=world_to_map(player.position)+direction
+	var obstacle=get_cellv(target_cell)
+	print(obstacle," ",target_cell)
+	if obstacle == 1:
+		return true
+	else:
+		return false
